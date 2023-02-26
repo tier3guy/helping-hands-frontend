f489c8ab23e1688e7fd3e2e3be84dadaf0d6c4db
Avinash Gupta <73595465+tier3guy@users.noreply.github.com>
2023-02-26 15:09:56 -0800

    API ...

---
+++ api/config.js
@@ -0,0 +1,19 @@
+/**
+ * @description - This is the mode of the app. It can be either 'development' or 'production'
+ * @type {String}
+ * @constant
+ * @default - 'development'
+ * @permissbleValues - 'development' | 'production'
+ */
+export const MODE = 'development';
+
+/**
+ * @description - This is the API URL of the app.
+ * @type {String}
+ * @constant
+ * @default - 'http://localhost:3000/api'
+ * @permissbleValues - 'http://localhost:3000/api' | 'https://gloock.herokuapp.com/api'
+ */
+export const TEST_API_URL = 'http://localhost:3000/api';
+export const PROD_API_URL = 'https://gloock.herokuapp.com/api';
+
---
+++ api/index.js
@@ -0,0 +1,12 @@
+export const SignupFunction = async (data) => {};
+
+export const LoginFunction = async (data) => {};
+
+export const LogoutFunction = async () => {};
+
+export const ResetPasswordFunction = async (data) => {};
+
+export const UpdatePasswordFunction = async (data) => {};
+
+export const UpdateProfileFunction = async (data) => {};
+
